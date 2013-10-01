efcdef5a1b5f8e4582a1c6b4e7801b854c738114
Christian Helmich <kagekirin@gmail.com>
2013-10-01 20:37:27 +0900

    added int, BEint types note: longs in MAXscript correspond to int32 in C++

--- sg_structs.h
+++ sg_structs.h
@@ -8,10 +8,16 @@ typedef unsigned short word;
 
 //data is big endian, hence we need to swap bytes
 //using BE* to keep track of which variables we need to swap before using
+typedef int		BEint;
 typedef long	BElong;
 typedef float	BEfloat;
 typedef word	BEhalf;	//half-precision float, has specific encoding
 typedef word	BEword;	//short
+
+//NOTE: 
+// MAXscript readlong reads 4 bytes, hence an int
+// cf. http://docs.autodesk.com/3DSMAX/15/ENU/MAXScript-Help/index.html?url=files/GUID-9014954D-C25C-45B5-BD9C-19260B852185.htm,topicNumber=d30e16813
+
 
 typedef void* unknown;	//temp filler so code compiles
 
